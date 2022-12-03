5459be1e0e5a4d48116e88c615040dfa6932487a
Killian ROYANT <killianr479@gmail.com>
2022-12-03 20:29:32 +0100

    add test route

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 const hostname = 'localhost'
 
+app.use('/static', express.static(path.join(__dirname, '/static')))
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/static/index.html'));
 })
+
 
 app.listen(port, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
